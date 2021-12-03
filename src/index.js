bd2c5498d399645d8efdb695b215270d63fa25e4
zakaria <zakariasamy10@gmail.com>
2021-12-03 21:07:43 +0200

    Init socket.io and test in via connecting to server in html page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const { Server } = require("socket.io");
 
 const app = express()
+
+// we uses http to pass what it return to socket.io because express dosen't return that
+const server = http.createServer(app)
+const io = new Server(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+// when the client connect, this achieved by calling js function in html page
+io.on('connection', () => {
+    console.log('New WebSocket connection')
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
