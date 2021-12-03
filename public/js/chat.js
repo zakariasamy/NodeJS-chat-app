d7108c8b03b95b39a6fc46e013991b3527e890d6
zakaria <zakariasamy10@gmail.com>
2021-12-03 21:46:07 +0200

    Example of trigger event from server to client and from client to server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+
+socket.on('printCount', (count) => {
+    console.log(`The count is ${count}`)
+})
+
+document.querySelector('#increment').addEventListener('click', () => {
+    socket.emit('increment')
+
+})
--- src/index.js
+++ src/index.js
@@ -14,9 +14,22 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+let count = 0
+
 // when the client connect, this achieved by calling js function in html page
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+
+    // Trigger event for the connected client
+    socket.emit('printCount', count)
+
+    // Handle client triggered event with the name (increment)
+    socket.on('increment', () => {
+        count++
+        // Trigger event for all clients
+        io.emit('printCount', count)
+    })
+
 })
 
 server.listen(port, () => {
